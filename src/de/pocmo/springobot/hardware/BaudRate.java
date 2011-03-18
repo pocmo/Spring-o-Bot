53e94b00a097c31ee3ac4ebf063734c012531a90
Sebastian Kaspari <s.kaspari@googlemail.com>
2011-03-19 00:54:01 +0100

    BaudRate: All Baud rates as defined in the Open Interface specification

---
+++ src/de/pocmo/springobot/hardware/BaudRate.java
@@ -0,0 +1,38 @@
+/*
+   Copyright 2011 Sebastian Kaspari
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package de.pocmo.springobot.hardware;
+
+/**
+ * Constants for the baud rates as defined in the Open Interface
+ * specification.
+ *
+ * @author Sebastian Kaspari <s.kaspari@googlemail.com>
+ */
+public class BaudRate
+{
+    public static int BAUD_300    = 0;
+    public static int BAUD_600    = 1;
+    public static int BAUD_1200   = 2;
+    public static int BAUD_2400   = 3;
+    public static int BAUD_4800   = 4;
+    public static int BAUD_9600   = 5;
+    public static int BAUD_14400  = 6;
+    public static int BAUD_19200  = 7;
+    public static int BAUD_28800  = 8;
+    public static int BAUD_38400  = 9;
+    public static int BAUD_57600  = 10;
+    public static int BAUD_115200 = 11;
+}
