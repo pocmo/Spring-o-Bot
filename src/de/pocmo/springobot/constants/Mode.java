72f97f39b19a291466aa077bbdfca2ad3a3f629c
Sebastian Kaspari <s.kaspari@googlemail.com>
2011-03-19 22:19:54 +0100

    Mode: Constants for Open Interface modes

---
+++ src/de/pocmo/springobot/constants/Mode.java
@@ -0,0 +1,29 @@
+/*
+   Copyright 2011 Sebastian Kaspari
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package de.pocmo.springobot.constants;
+
+/**
+ * Constants for Open Interface modes.
+ *
+ * @author Sebastian Kaspari <s.kaspari@gmail.com>
+ */
+public class Mode
+{
+    public static final int OFF = 0;
+    public static final int PASSIVE = 1;
+    public static final int SAFE = 2;
+    public static final int FULL = 3;
+}
