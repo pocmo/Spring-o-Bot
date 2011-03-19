bd854a412156d0e4202585b745e2722f6e8a40bf
Sebastian Kaspari <s.kaspari@googlemail.com>
2011-03-19 12:38:00 +0100

    ChargingState: Constants for the charging state as returned by the charging sensor

---
+++ src/de/pocmo/springobot/hardware/ChargingState.java
@@ -0,0 +1,32 @@
+/*
+   Copyright 2011 Sebastian Kaspari
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package de.pocmo.springobot.hardware;
+
+/**
+ * Constants for the charging state as returned by the charging
+ * sensor.
+ *
+ * @author Sebastian Kaspari <s.kaspari@googlemail.com>
+ */
+public class ChargingState
+{
+    public static int NOT_CHARGING = 0;
+    public static int RECONDITIONING_CHARGING = 1;
+    public static int FULL_CHARGING = 2;
+    public static int TRICKLE_CHARGING = 3;
+    public static int WAITING = 4;
+    public static int CHARGING_FAULT_CONDITION = 5;
+}
